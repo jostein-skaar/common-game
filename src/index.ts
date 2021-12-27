55f3b030df3867b24bc4b671bc882a4276de9343
Jostein Skaar <jostein.skaar@gmail.com>
2021-12-27 15:21:55 +0100

    Export from index

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { DimensionLimits } from './dimension/dimension-limits.model';
 export { DimensionResult } from './dimension/dimension-result.model';
 export { ScaleMode } from './dimension/scale-mode.enum';
 export { calculateDimension } from './dimension/calculate-dimension';
+export { reloadWhenResize } from './reload-when-resize';
