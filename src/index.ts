33b6ed46e3ba9ecece68928a2337f5f44bf2ad5f
Jostein Skaar <jostein.skaar@gmail.com>
2022-05-26 13:36:48 +0200

    Added helper for Tiled maps

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { DimensionResult } from './dimension/dimension-result.model';
 export { ScaleMode } from './dimension/scale-mode.enum';
 export { calculateDimension } from './dimension/calculate-dimension';
 export { reloadWhenResize } from './reload-when-resize';
+export { changePixelRatioInTiledJson } from './tiled-map-helper';
---
+++ src/tiled-map-helper.ts
@@ -0,0 +1,27 @@
+import * as fs from 'fs';
+
+export function changePixelRatioInTiledJson(pixelRatio: number, srcPath: string, dstPath: string) {
+  console.log('handlePixelRatio for: ', pixelRatio);
+  let mapContent = fs.readFileSync(srcPath);
+  let map = JSON.parse(mapContent.toString());
+  changeToPixelRatio(map, pixelRatio);
+
+  let newMapContent = JSON.stringify(map, null, 2);
+  fs.writeFileSync(dstPath, newMapContent);
+}
+
+function changeToPixelRatio(map: any, pixelRatio: number) {
+  map.tilewidth *= pixelRatio;
+  map.tileheight *= pixelRatio;
+
+  for (const tileset of map.tilesets) {
+    tileset.imagewidth *= pixelRatio;
+    tileset.imageheight *= pixelRatio;
+    tileset.tilewidth *= pixelRatio;
+    tileset.tileheight *= pixelRatio;
+    tileset.margin *= pixelRatio;
+    tileset.spacing *= pixelRatio;
+    tileset.image = tileset.image.replace('@1', `@${pixelRatio}`);
+    tileset.name = tileset.name.replace('@1', `@${pixelRatio}`);
+  }
+}
