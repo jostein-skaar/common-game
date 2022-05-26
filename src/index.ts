9d5a1fd951159f84808bb67a667e46576b22fa7b
Jostein Skaar <jostein.skaar@gmail.com>
2022-05-26 19:33:37 +0200

    Adding .js in index.js exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-// export { DimensionLimits } from './dimension/dimension-limits.model';
-// export { DimensionResult } from './dimension/dimension-result.model';
-// export { ScaleMode } from './dimension/scale-mode.enum';
-export { calculateDimension } from './dimension/calculate-dimension';
-export { reloadWhenResize } from './reload-when-resize';
-export { changePixelRatioInTiledJson } from './tools/tiled-map-helper';
+export { DimensionLimits } from './dimension/dimension-limits.model.js';
+export { DimensionResult } from './dimension/dimension-result.model.js';
+export { ScaleMode } from './dimension/scale-mode.enum.js';
+export { calculateDimension } from './dimension/calculate-dimension.js';
+export { reloadWhenResize } from './reload-when-resize.js';
+export { changePixelRatioInTiledJson } from './tools/tiled-map-helper.js';
