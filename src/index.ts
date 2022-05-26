26d677f61155c749b9fe8020426db78235496266
Jostein Skaar <jostein.skaar@gmail.com>
2022-05-26 19:26:22 +0200

    Try removing types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export { DimensionLimits } from './dimension/dimension-limits.model';
-export { DimensionResult } from './dimension/dimension-result.model';
-export { ScaleMode } from './dimension/scale-mode.enum';
+// export { DimensionLimits } from './dimension/dimension-limits.model';
+// export { DimensionResult } from './dimension/dimension-result.model';
+// export { ScaleMode } from './dimension/scale-mode.enum';
 export { calculateDimension } from './dimension/calculate-dimension';
 export { reloadWhenResize } from './reload-when-resize';
 export { changePixelRatioInTiledJson } from './tools/tiled-map-helper';
