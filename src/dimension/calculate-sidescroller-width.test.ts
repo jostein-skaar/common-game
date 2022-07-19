c6eda4eb9f7ad2ccdc9c8bec306fc9f8312532ca
Jostein Skaar <jostein.skaar@gmail.com>
2022-07-19 21:40:09 +0200

    Trying hard

--- src/dimension/calculate-sidescroller-width.test.ts
+++ src/dimension/calculate-sidescroller-width.test.ts
@@ -39,37 +39,37 @@ describe('calculateSidescrollerWidth', () => {
   // Height is bound to the number of tiles in a sidescroller. 18 tiles x 32 px = 576 px.
   const height = 576;
   const maxWidth = 1024;
+  const maxHeight = 1024;
   const maxWidthMap = 100 * 32;
 
   const dimensjoner = [
     [300, 300, height],
     [1000, 1000, height],
     [2000, 2000, height],
+
     // Portrait
     [100, 288, 50],
     [375, 812, 266.00985221674875], // iPhone XS
     [768, 1024, 432], // iPad stående
     [360, 640, 324], // Mest populær mobil-størrelse
     [375, 667, 323.8380809595202], // Nest mest populær mobil-størrelse
-    [800, 1280, 360], // Nest mest populær tablet
-    //[300, 2000, 86.39999999999999], // Halvsprø tilfeller
-    //[10, 2000, 2.88], // Sprø tilfeller
-    // [768, 1004, 768, 1004, ScaleMode.None], // Faktisk iPad stående (har jo statuslinje)
-    // [768, 954, 763.2, 954, ScaleMode.None], // Faktisk iPad stående i Safari (har jo statuslinje og navbar?)
+    [800, 1280, 450], // Nest mest populær tablet
+    [300, 2000, 168.75], // Halvsprø tilfeller
+    [50, 2000, 28.125], // Sprø tilfeller
+    [768, 1004, 440.60557768924303], // Faktisk iPad stående (har jo statuslinje)
+    [768, 954, 463.6981132075472], // Faktisk iPad stående i Safari (har jo statuslinje og navbar?)
 
     // Landscape
     [1200, 288, 512],
-    [1920, 1080, 1024], //
-    [1800, 1000, 1024], //
-    // [568, 320, 315.55555555555554], //
+    [1920, 1080, 576], // PC
+    [1800, 1000, 589.824],
     // [1366, 768, 768], // Landscape. Mest populær desktop-størrelse
     // [1366, 700, 640, 800, ScaleMode.Fit], // Mammas Chromebook
     // [812, 375, 640, 800, ScaleMode.Fit], // iPhone XS liggende
     // [1024, 768, 640, 800, ScaleMode.Fit], // iPad liggende
     // [1280, 800, 640, 800, ScaleMode.None], // Nest mest populær tablet
-
-    // [1920, 1080, 768, 1024, ScaleMode.None], // PC
-
+    [300, 301, 576],
+    // [568, 320, 315.55555555555554], //
     // [2000, 300, 640, 800, ScaleMode.Fit], // Halvsprø tilfeller
 
     // [2000, 10, 640, 800, ScaleMode.Fit], // Sprø tilfeller
@@ -81,7 +81,7 @@ describe('calculateSidescrollerWidth', () => {
   ];
 
   test.each(dimensjoner)('gitt %p og %p som argumenter, returnerer %p, %p og %p', (availableWidth, availableHeight, expectedWidth) => {
-    const actualWidth = calculateSidescrollerWidth(height, maxWidth, maxWidthMap, availableWidth, availableHeight);
+    const actualWidth = calculateSidescrollerWidth(height, maxWidth, maxHeight, maxWidthMap, availableWidth, availableHeight);
     expect(actualWidth).toEqual(expectedWidth);
   });
 });
--- src/dimension/calculate-sidescroller-width.ts
+++ src/dimension/calculate-sidescroller-width.ts
@@ -1,6 +1,7 @@
 export function calculateSidescrollerWidth(
   height: number,
   maxWidth: number,
+  maxHeight: number,
   maxWidthMap: number,
   availableWidth: number,
   availableHeight: number
@@ -11,13 +12,19 @@ export function calculateSidescrollerWidth(
 
   const isLandscape = availableWidth > availableHeight;
   const isPortrait = availableWidth < availableHeight;
+
+  if (availableHeight > maxHeight) {
+    availableHeight = maxHeight;
+  }
+
   const scale = availableHeight / height;
 
   console.log('scale', scale, availableHeight, height);
 
-  if (scale < 1 && isLandscape) {
-    let width = availableWidth * scale;
+  if (scale < 1) {
     const maxWidthScaled = maxWidth * scale;
+    let width = availableWidth * scale;
+    console.log('width calc', availableWidth, scale);
     if (width > maxWidthScaled) {
       width = maxWidthScaled;
     }
@@ -27,34 +34,12 @@ export function calculateSidescrollerWidth(
     return width;
   }
 
-  if (scale < 1 && isPortrait) {
-    const maxWidthScaled = maxWidth * scale;
-    let width = availableWidth * scale;
+  if (scale > 1) {
+    const maxWidthScaled = maxWidth / scale;
+    let width = availableWidth / scale;
+
     if (width > maxWidthScaled) {
       width = maxWidthScaled;
-    }
-    if (width > maxWidthMap) {
-      width = maxWidthMap;
-    }
-    return width;
-  }
-
-  if (scale > 1 && isLandscape) {
-    let width = availableWidth / scale;
-
-    if (width > maxWidth) {
-      width = maxWidth;
-    }
-    if (width > maxWidthMap) {
-      width = maxWidthMap;
-    }
-    return width;
-  }
-
-  if (scale > 1 && isPortrait) {
-    let width = availableWidth / scale;
-    if (width > maxWidth) {
-      width = maxWidth;
     }
     if (width > maxWidthMap) {
       width = maxWidthMap;
