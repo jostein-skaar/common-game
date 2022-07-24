9f35ece5c574a74bb9ddb799c62360b274adea4e
Jostein Skaar <jostein.skaar@gmail.com>
2022-07-24 16:42:53 +0200

    Pixelratio ceil for handeling fractions

--- src/pixel-ratio-fix.ts
+++ src/pixel-ratio-fix.ts
@@ -3,15 +3,17 @@ declare global {
 }
 
 let pixelRatio = 1;
-// Check fow window because this lib may be run in NodeJS as well.
+// Check for window because this lib may be run in NodeJS as well.
 if (typeof window !== 'undefined') {
-  pixelRatio = window.devicePixelRatio;
-  if (pixelRatio !== 1 && pixelRatio !== 2 && pixelRatio !== 3) {
+  pixelRatio = Math.ceil(window.devicePixelRatio);
+  if (pixelRatio < 1) {
     pixelRatio = 1;
+  } else if (pixelRatio > 3) {
+    pixelRatio = 3;
   }
 }
-// globalThis.pixelRatio = pixelRatio;
-globalThis.pixelRatio = 3;
+
+globalThis.pixelRatio = pixelRatio;
 
 export function adjustForPixelRatio(size: number): number {
   return size * globalThis.pixelRatio;
